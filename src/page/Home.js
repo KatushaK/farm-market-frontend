2dc8ba9bc54e675f7699094b4c3fdf6864f94763
KatushaK <141676047+KatushaK@users.noreply.github.com>
2024-02-23 12:40:10 -0500

    Updated key on Home.js

--- src/page/Home.js
+++ src/page/Home.js
@@ -23,7 +23,7 @@ function Home() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await fetch(`${process.env.REACT_APP_SERVER_DOMAIN}/product`);
+        const res = await fetch(`https://farm-market-backend.onrender.com/product`);
         const resData = await res.json();
         dispatch(setDataProduct(resData)); 
       } catch (error) {
